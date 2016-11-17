225f09037bfad184437c76563f9dcb6c70792ca1
Dan Engelbrecht <dan.engelbrecht@autodesk.com>
2016-11-17 17:00:01 +0100

    Changed so we use same strategy when renaming dll as for pdb to handle the case when the dll does not actually support hot-reloading if which case the .dll.old file would continue to be locked.

--- engine_plugins/clean-dlls-and-pdbs.bat
+++ engine_plugins/clean-dlls-and-pdbs.bat
@@ -21,27 +21,26 @@ set clean_time=%clean_time:,=_%
 set dll_plugin_path=%plugin_output_folder%%plugin_base_name%.dll
 set pdb_plugin_path=%plugin_output_folder%%plugin_base_name%.pdb
 
-set old_dll_plugin_name=%plugin_base_name%.dll.old
+set old_dll_plugin_name=%plugin_base_name%.%clean_time%.dll.old
 set old_pdb_plugin_name=%plugin_base_name%.%clean_time%.pdb.old
 
 set old_dll_plugin_path=%plugin_output_folder%%old_dll_plugin_name%
 set old_pdb_plugin_path=%plugin_output_folder%%old_pdb_plugin_name%
+set old_dll_plugin_wildcard=%plugin_output_folder%%plugin_base_name%.*.dll.old
 set old_pdb_plugin_wildcard=%plugin_output_folder%%plugin_base_name%.*.pdb.old
 
-del %old_dll_plugin_path% >nul 2>nul
+del %old_dll_plugin_wildcard% >nul 2>nul
 del %old_pdb_plugin_wildcard% >nul 2>nul
 
 if exist %dll_plugin_path% (
-	echo moving %dll_plugin_path% -> %old_dll_plugin_name%
 	rename %dll_plugin_path% %old_dll_plugin_name%
 )
 
 if exist %pdb_plugin_path% (
-	echo moving %pdb_plugin_path% -> %old_pdb_plugin_name%
 	rename %pdb_plugin_path% %old_pdb_plugin_name%
 )
 
-del %old_dll_plugin_path% >nul 2>nul
+del %old_dll_plugin_wildcard% >nul 2>nul
 del %old_pdb_plugin_wildcard% >nul 2>nul
 
 goto end
